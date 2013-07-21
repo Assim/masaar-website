41e125f93ab2235366d99ddf18f882676462dd2e
Assim Al-Marhuby <assim1991@gmail.com>
2013-07-22 02:10:10 +0400

    First commit

---
+++ index.php
@@ -0,0 +1,445 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	
+	<title>Masaar for Marketing Solutions</title>
+	<meta name="description" content="Masaar is a private company founded by twenty ambitious Omani youth, our number one priority is to offer fast and reliable marketing solutions.">
+	<meta name="keywords" content="Shopping,Oman,Omani,Innovation,Masaar,Students,HCT,Young,Smart,Higher College of Technology,Muscat">
+	<meta name="author" content="Assim Al-Marhuby">
+	<meta name="HandheldFriendly" content="true">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Import CSS -->
+	<link rel="stylesheet" href="css/base.css">
+	<!--[if lt IE 10]>
+		<link href= "css/ie.css" rel= "stylesheet" media= "all" />
+	<![endif]-->
+	
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>	
+	<script src="js/center.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+
+	<!-- Start Header Slide -->
+	<header id="home">
+		<div class="pattern"></div>
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					
+					<!-- Start Top Navigation -->
+					<nav id="topnav">
+						<div class="row">
+							<div class="span5">
+								<a href="index.html"><img class="logo" src="img/logo.png" alt=""></a>
+							</div>
+							<div class="span7">
+								<ul id="fixed-nav" class="hidden-phone scroll-links">
+									<li class="current"><a href="#home">Home</a></li>
+									<li><a href="#about">About Us</a></li>
+									<li><a href="#team">Team</a></li>
+									<li><a href="#services">Services</a></li>
+									<li><a href="#contact">Contact Us</a></li>
+								</ul>
+								<ul class="social-icons">
+									<li><a target="_blank" href="http://www.facebook.com/MasaarOman">&#228;</a></li>
+									<li><a target="_blank" href="http://www.twitter.com/MasaarOman">&#229;</a></li>
+									<!--li><a target="_blank" href="http://plus.google.com/117343523319016151408">&#232;</a></li-->
+									<li><a target="_blank" href="http://www.instagram.com/MasaarOman">&#235;</a></li>
+									<!--li><a target="_blank" href="http://www.youtube.com/MasaarOman">&#240;</a></li-->
+									<li><a target="_blank" href="http://www.linkedin.com/company/3120443">&#231;</a></li>
+								</ul>
+							</div>
+						</div>
+					</nav>
+					<!-- End Header Slide -->
+					
+					<div id="slogan-text" class="slogan-text">
+						<div class="slogan">Shopping has never been <span class="red-text">easier</span>.</div>
+						<div class="maintext">We are working hard to offer a <strong><span class="white-text">service</span></strong> that will <strong><span class="red-text">change</span></strong> the way you shop by making it easier.</div>
+					</div>
+					
+					<script>
+						$(window).resize();
+					</script>
+					
+					<div class="scroll-links">
+						<a href="#about" class="nextslide"><span>Who we are?</span></a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- End Header Slide -->
+	
+	<div class="wrapper">
+		
+		<!-- Start Main Navigation -->
+		<nav id="mainnav">
+			<div class="container">
+				<div class="row">
+					<div class="span5">
+						<a href="index.html"><img class="logo" src="img/logo-small.png" alt=""></a>
+					</div>
+					<div class="span7">
+						<ul id="fluid-nav" class="fluid-navigation hidden-phone">
+							<li class="current"><a href="#home">Home</a></li>
+							<li><a href="#about">About Us</a></li>
+							<li><a href="#team">Team</a></li>
+							<li><a href="#services">Services</a></li>
+							<li><a href="#contact">Contact Us</a></li>
+						</ul>
+								<ul class="social-icons">
+									<li><a target="_blank" href="http://www.facebook.com/MasaarOman">&#228;</a></li>
+									<li><a target="_blank" href="http://www.twitter.com/MasaarOman">&#229;</a></li>
+									<!--li><a target="_blank" href="http://plus.google.com/117343523319016151408">&#232;</a></li-->
+									<li><a target="_blank" href="http://www.instagram.com/MasaarOman">&#235;</a></li>
+									<!--li><a target="_blank" href="http://www.youtube.com/MasaarOman">&#240;</a></li-->
+									<li><a target="_blank" href="http://www.linkedin.com/company/3120443">&#231;</a></li>
+								</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<!-- End Main Navigation -->
+	
+		<!-- Start About Page -->
+		<section id="about" class="page">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+							<div class="title">About <span class="red-text">Masaar</span></div>
+						<div class="lead">Masaar is a private company founded by twenty ambitious Omani youth, our number one priority is to offer fast and reliable marketing solutions. Innovation is a major part of Masaar's objectives. Emphasis placed on the development and support of marketing strategy of our clients and solutions designed to simplify the lives of consumers by performing research on tomorrow’s marketing solutions and works with various clients to develop solutions from start to finish - from the initial concept to market readiness.</div>
+
+						<div class="row two-columns">
+							<div class="span6">
+ 								<div class="lead">Our <span class="red-text">Mission</span></div>
+								<p class="about-text">To support our clients marketing plan by providing easy, fast, and credible solutions to customers.</p>
+							</div>
+							<div class="span6">
+ 								<div class="lead">Our <span class="red-text">Vision</span></div>
+								<p class="about-text">To be revolutionary shopping solutions company globally.</p>
+
+							</div>
+						</div>
+						<div class="row two-columns">
+							<div class="span6">
+ 								<div class="lead">Our <span class="red-text">Values</span></div>
+								<p class="about-text">Our values guide our actions to ensure that we all have a common understanding of what we can expect from each other. Our values are:<br/><br/>Respect, Loyalty, Quality, Creating Wealth, Creativity, Fair Treatment, Teamwork, Reliability, Originality, Safety, Honesty</p>
+							</div>
+							<div class="span6">
+ 								<div class="lead"><span class="red-text">Why</span> choose us?</div>
+								<p class="about-text">There is no question that more than enough of a reason to choose us is because we are the first company in Oman established by a group of young minds, students of this country that seek for a better future of their beloved country.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About Page -->
+		
+		<!-- Start Team Page -->
+		<section id="team" class="page">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						<div class="title"><span class="red-text">Who</span> we are?</div>
+						<p class="lead">We are a group of proud Omani students, with rich knowledge coming from different specializations such as Information Technology, Engineering, and Finance; representing a strong base of an upcoming and growing company.</p>
+						<div class="row">
+						
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/sulaiman-almahrooqi.jpg" alt="">
+								<div class="profile-name">Sulaiman Al-Mahrooqi</div>
+								<div class="profile-job">Chief Executive Officer<br/>&nbsp;</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/mohsin.jpg" alt="">
+								<div class="profile-name">Mohsin Al-Zadjali</div>
+								<div class="profile-job">Vice President<br/>&nbsp;</div>
+							</div>
+							<!-- End Profile -->
+							
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/assim.jpg" alt="">
+								<div class="profile-name">Assim Al-Marhuby</div>
+								<div class="profile-job">Production Manager<br/>Production Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/ghadeer.jpg" alt="">
+								<div class="profile-name">Ghadeer Al-Hajry</div>
+								<div class="profile-job">Marketing Manager<br/>Marketing Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/ahlam.jpg" alt="">
+								<div class="profile-name">Ahlam Al-Alawi</div>
+								<div class="profile-job">Public Relations Manager<br/>Public Relations Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/saeed.jpg" alt="">
+								<div class="profile-name">Saeed Al-Shaqsi</div>
+								<div class="profile-job">Research & Development Manager<br/>Research & Development Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/hesham.jpg" alt="">
+								<div class="profile-name">Hesham Al-Khanjary</div>
+								<div class="profile-job">Finance Manager<br/>Finance Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/hamad.jpg" alt="">
+								<div class="profile-name">Hamad Al-Mahrooqi</div>
+								<div class="profile-job">Human Resources Manager<br/>Human Resources Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/amal.jpg" alt="">
+								<div class="profile-name">Amal Al-Jabri</div>
+								<div class="profile-job">System Analyst<br/>Production Department</div>
+							</div>
+							<!-- End Profile -->
+
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/sharifa.jpg" alt="">
+								<div class="profile-name">Sharifa Al-Abri</div>
+								<div class="profile-job">Computer Engineer<br/>Production Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/ali.jpg" alt="">
+								<div class="profile-name">Ali Al-Maskary</div>
+								<div class="profile-job">Marketing Analyst/Network Engineer<br/>Marketing/Production Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/sarah.jpg" alt="">
+								<div class="profile-name">Sarah Al-Harthy</div>
+								<div class="profile-job">Communications Coordinator<br/>Public Relations Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/thuraya.jpg" alt="">
+								<div class="profile-name">Thuraya Al-Balushi</div>
+								<div class="profile-job">Events Coordinator<br/>Public Relations Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/ghalia.jpg" alt="">
+								<div class="profile-name">Ghalia Al-Habsi</div>
+								<div class="profile-job">Data Analyst<br/>Research & Development Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/manhal.jpg" alt="">
+								<div class="profile-name">Manhal Al-Hooti</div>
+								<div class="profile-job">System Developer<br/>Research & Development Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/alaa.jpg" alt="">
+								<div class="profile-name">Alaa Al-Saleh</div>
+								<div class="profile-job">Finance Analyst<br/>Finance Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/khulood.jpg" alt="">
+								<div class="profile-name">Khulood Al-Esry</div>
+								<div class="profile-job">Procurement Officer<br/>Finance Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/azza.jpg" alt="">
+								<div class="profile-name">Azza Al-Ghaithi</div>
+								<div class="profile-job">Employee Relations Officer<br/>Human Resources Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/haitham.jpg" alt="">
+								<div class="profile-name">Haitham Al-Lamki</div>
+								<div class="profile-job">HR Officer<br/>Human Resources Department</div>
+							</div>
+							<!-- End Profile -->
+
+							<!-- Start Profile -->
+							<div class="span3 profile">
+								<img src="img/team/sulaiman-alshaqsi.jpg" alt="">
+								<div class="profile-name">Sulaiman Al-Shaqsi</div>
+								<div class="profile-job">HR Administrator<br/>Human Resources Department</div>
+							</div>
+							<!-- End Profile -->
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Team Page -->
+		
+		<!-- Start Services Page -->
+		<section id="services" class="page">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						<div class="title"><span class="red-text">Our</span> services</div>
+						<div class="lead">Easy Electronic Shopping System (EESS): It is our first product produced by our company, EESS is a revolutionary life changing invention, that will make people's lives more easier, by providing more comfortable, timesaving and a faster way of shopping.</div>
+						<hr class="thick">
+						<div class="row">
+							<div class="span6">
+								<div class="service-title"><span class="service-icon">&#0115;</span>Problem</div>
+								<div class="service-text">
+									<p>We have made a market survey and we found out that a huge number of people are busy and don't have time to shop, they prefer someone else to do the job for them.</p>
+									<hr class="white visible-phone">
+								</div>
+							</div>
+							<div class="span6">
+								<div class="service-title"><span class="service-icon">&#0035;</span>Solution</div>
+								<div class="service-text">
+									<p>That is when EESS came, a service that will soon be released in the market to help you shop easily, and you don't even have to do the shopping by yourself. Stay tuned and you will be seeing us around.</p>
+									<hr class="white visible-phone">
+								</div>
+							</div>
+						</div>
+						<hr class="white">
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Services Page -->
+		
+		<!-- Start Twitter Feed -->
+		<section id="twitter">
+			<div class="container">
+				<div id="twitter-feed" class="row">
+				</div>
+			</div>
+		</section>
+		<!-- End Twitter Feed -->
+		
+		<!-- Start Contact Page -->
+		<section id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						<div class="title">Contact Us</div>
+						<div class="contact-text">If you have anything you'd like to tell us, then feel free to contact us.</div>
+						<address>
+							<div class="email">contact@masaaroman.com</div>
+							<div class="phone">+968 95621443</div>
+							<div class="address">Muscat, Oman</div>
+						</address>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Contact Page -->
+		
+		<!-- Start Feedback -->
+		<section id="feedback">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+					
+						<div class="title">Feedback form</div>
+						<div class="result">
+						</div>
+						
+						<!-- Start Form -->
+						<form class="well">
+							<div class="row">
+								<div class="span6 pull-left">
+									<label>Your name</label>
+									<input class="span6" type="text" name="name" placeholder="Your Name">
+								</div>
+								<div class="span6 pull-right">
+									<label>Your e-mail</label>
+									<input class="span6" type="text" name="email" placeholder="Your E-mail">
+								</div>
+							</div>
+							<div class="row">
+								<div class="span12 textarea-margin">
+									<label>Message</label>
+									<textarea class="span12" rows="6" name="message" placeholder="Message"></textarea>
+									<button type="submit" class="btn rounded dark"><i class="web-icon">8</i>Send Message</button>
+								</div>
+							</div>
+						</form>
+						<!-- End Form -->
+						
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Feedback -->
+		
+		
+		<section id="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="span12">&copy;2013 <strong>Masaar for Marketing Solutions</strong>.</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<script src="js/jquery.sticky.js"></script><!-- Sticky Navigation -->
+	<script src="js/selectnav.min.js"></script><!-- Mobile Navigation -->
+	<script src="js/jquery.scrollTo.js"></script><!-- Smooth Scrolling -->
+	<script src="js/jquery.nav.js"></script><!-- One Page Navigation -->
+	<script src="js/contact.js"></script><!-- Contact Form -->
+	<script src="js/settings.js"></script><!-- Settings -->
+	<script src="js/last-tw.js"></script><!-- Twitter Feed -->
+	<script src="https://api.twitter.com/1/statuses/user_timeline/masaaroman.json?callback=twitterCallback2&count=3"></script>
+
+</body>
+
+</html>
